78a5ca7dc5d705859353de5e63487a589d6d321a
LukaKoporec <luka.koporec123@gmail.com>
2021-10-13 06:35:03 +0200

    routes

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 
+app.get("/", function(req, res){
+    res.send("<h1>Hello</h1>");
+});
+
+app.get("/contact", function(req, res){
+    res.send("Contact me at: ....");
+});
+
+app.get("/about", function(req, res){
+    res.send("My name is Luka...");
+});
+
+app.get("/hobbies", function(req, res){
+    res.send("none");
+});
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
